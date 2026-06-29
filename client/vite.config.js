446f395e5a5a7734c1c11dd30a076ba4bca25204
Jheiksouza <jheikhector@gmail.com>
2026-06-29 00:43:40 -0300

    Fix: HTTPS no Vite (@vitejs/plugin-basic-ssl) para geolocation funcionar no iOS

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   envDir: '..',
   build: {
     outDir: 'dist'
   },
   server: {
+    https: true,
     host: '0.0.0.0',
     port: 5173,
     proxy: {
