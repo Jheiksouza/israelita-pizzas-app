6635a319ed4e76fda346d1b02b91bbfe4f4b4bbc
Jheiksouza <jheikhector@gmail.com>
2026-07-07 22:01:14 -0300

    fix: chain getPlatformStatuses through index.js

--- marketplaces/index.js
+++ marketplaces/index.js
@@ -1,11 +1,11 @@
-const { register, getAdapter, getAllAdapters, getConfigDefaults, getPlatformInfo } = require('./shared/registry')
+const { register, getAdapter, getAllAdapters, getConfigDefaults, getPlatformInfo, getPlatformStatuses } = require('./shared/registry')
 const IfoodAdapter = require('./ifood/adapter')
 const NinetyNineFoodAdapter = require('./ninetyNineFood/adapter')
 
 function setup() {
   register(new IfoodAdapter())
   register(new NinetyNineFoodAdapter())
-  return { getAdapter, getAllAdapters, getConfigDefaults, getPlatformInfo }
+  return { getAdapter, getAllAdapters, getConfigDefaults, getPlatformInfo, getPlatformStatuses }
 }
 
 module.exports = setup
