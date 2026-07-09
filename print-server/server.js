d53a4d2b77f1f1610fcd4a9f47270c256a2d4df5
Jheiksouza <jheikhector@gmail.com>
2026-07-08 22:49:01 -0300

    fix: adiciona CORS ao print server para aceitar requisicoes do Vercel

--- print-server/server.js
+++ print-server/server.js
@@ -8,6 +8,13 @@ const PRINTER_NAME = process.env.PRINTER_NAME || null
 
 const app = express()
 app.use(express.json({ limit: '1mb' }))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+  if (req.method === 'OPTIONS') return res.sendStatus(204)
+  next()
+})
 
 function cp850(buf) {
   const map = {
